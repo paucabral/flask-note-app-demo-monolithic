cf3842198fb38951a8d1bcc8db1fd7c9485d8e46
paucabral <josepaucabral@gmail.com>
2023-06-17 18:20:17 +0800

    feat: initial commit, instantiate model

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+if __name__ == "__main__":
+    app.run(debug=True)
