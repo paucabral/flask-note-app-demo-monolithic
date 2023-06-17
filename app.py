41366c224159bf66e6dc49f14415bf8a31dfd10d
paucabral <josepaucabral@gmail.com>
2023-06-17 21:32:23 +0800

    add read and delete functionalities

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
@@ -19,9 +19,36 @@ class NoteSchema(ma.Schema):
 note_schema = NoteSchema()
 notes_schema = NoteSchema(many=True)
 
+@app.route('/notes', methods=['POST'])
+def create_note():
+    data = {
+        "title": request.form["title"],
+        "content": request.form["content"]
+    }
+
+    note = Note(title=data["title"], content=data["content"])
+    db.session.add(note)
+    db.session.commit()
+    return redirect('/')
+
 @app.route('/')
 def get_all_notes():
-    return render_template('index.html')
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes)
+
+@app.route('/notes/<int:note_id>')
+def get_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    return render_template("note.html", note=note)
+
+@app.route('/notes/<int:note_id>', methods=['POST'])
+def update_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    data = request.form
+    note.title = data['title']
+    note.content = data['content']
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     with app.app_context():
